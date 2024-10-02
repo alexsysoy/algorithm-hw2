7f07f926ac9fa5d114d7dd97f6176f4afe0bb8dd
alexsysoy <alexsysoy@yandex.ru>
2024-10-02 22:42:38 +0300

    add hw2

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 
 public class HomeWork extends BinaryIntSearchTree {
@@ -15,17 +17,60 @@ public class HomeWork extends BinaryIntSearchTree {
      * коллекция в дереве 1, 2, 3, 4, 5
      * count = 3, upperBound 4
      * ответ [4, 3, 2]
-
      *
-     * @param count максимальное количество элементов в ответе
+     * @param count      максимальное количество элементов в ответе
      * @param upperBound верхняя граница для поиска элементов
      * @return массив найденных максимальных значений не более чем upperBound и длиной не более count, отсортировано от большего к меньшему
      * Сигнатуру метода не меняем
      */
     public List<Integer> findMaxDigits(int count, int upperBound) {
-        //TODO реализовать метод
-        return null;
+        List<Integer> list = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+
+        stack.push(root);
+
+        while (!stack.empty()) {
+            Node current = stack.pop();
+            if (current == null) {
+                continue;
+            }
+            if (list.contains(current.value)) {
+                continue;
+            }
+            if (current.left != null) {
+                if (!list.contains(current.left.value)) {
+                    stack.push(current);
+                    stack.push(current.left);
+                    continue;
+                }
+                list.add(current.value);
+            }
+            if (current.right != null) {
+                if (!list.contains(current.value)) {
+                    list.add(current.value);
+                }
+                stack.push(current.right);
+                continue;
+            }
+            if (current.left == null) {
+                list.add(current.value);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>(List.of());
+
+        if (list.size() > 1) {
+            for (int i = list.size() - 1; i > 0; i--) {
+                if (list.get(i) <= upperBound && count > 0) {
+                    result.add(list.get(i));
+                    count--;
+                }
+            }
+        }
+        if (list.size() == 1 && list.get(0) <= upperBound && count > 0) {
+            result.add(list.get(0));
+        }
+
+        return result;
     }
-
-
 }
--- src/test/java/org/example/BinaryIntSearchTreeTest.java
+++ src/test/java/org/example/BinaryIntSearchTreeTest.java
@@ -1,6 +1,12 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +22,39 @@ class BinaryIntSearchTreeTest {
             tree.add(i);
         }
         tree.add(10);
+    }
+
+    // -1 означает отсутствие ответа
+    @ParameterizedTest(name = "digits {0} count {1} upperbound {2} expected {3}")
+    @CsvSource(value = {
+            "7,4,5,6,1,10,9:3:7:7,6,5",
+            "1,2,3,4,5:3:4:4,3,2",
+            "7,4,5,6,1,10,9,15,14,20,21,22,19:6:14:14,10,9,7,6,5",
+            "7,4,5,6,1,10,9,15,14,20,21,22,19:3:14:14,10,9",
+            "10:1:10:10",
+            "10:5:10:10",
+            "10:1:9:-1"
+    }
+            , delimiter = ':')
+    void findMaxDigitsTest(String digits, int count, int upperBound, String expected) {
+        HomeWork homeWork = new HomeWork();
+        addFromString(digits, homeWork);
+
+        assertEquals(createList(expected), homeWork.findMaxDigits(count, upperBound));
+    }
+
+    private void addFromString(String digits, HomeWork tree) {
+        List<Integer> list = Arrays.stream(digits.split(","))
+                .map(Integer::valueOf).collect(Collectors.toList());
+        list.forEach(tree::add);
+    }
+
+    private List<Integer> createList(String integers) {
+        if (integers.equals("-1")) {
+            return List.of();
+        }
+        return Arrays.stream(integers.split(","))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 }
